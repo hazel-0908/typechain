bd5f632d9fa2b6022de1f6ebc04e66d0b7a6febe
hanheesong <gmlthd0908@gmail.com>
2018-12-12 12:03:23 +0900

    Setting Typescript Up

---
+++ index.ts
@@ -0,0 +1,13 @@
+//console.log("hello");
+
+const name = "Poburi",
+    age = 24,
+    gender = "female";
+
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you ar ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age, gender);
+
+export {};
